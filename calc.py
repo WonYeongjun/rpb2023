9ac2ce03b4af7d9e319caa0f534a242ab9ad4e27
WonYeongjun <1014wonyoung@snu.ac.kr>
2024-04-09 23:30:54 +0900

    a
    Signed-off-by: WonYeongjun <1014wonyoung@snu.ac.kr>

--- calc.py
+++ calc.py
@@ -1,5 +1,20 @@
+def add(a,b):
+    return a+b
+def multiply(a,b):
+    return a*b
 def main():
-  print("We are subteam B")
-
+    print("This is calculator")
+    
+    print("Choose your operator as a number (1=add, 2=minus, 3=multiply, 4=divide)")
+    opttype = int(input("> "))
+    print("You chose ")
+    if (opttype == 1):
+        print("addition.")
+    elif (opttype==2):
+        print("minus")
+    elif (opttype==3):
+        print("multiply")
+    else:
+        print("divide")
 if __name__ == "__main__":
-  main()
+    main()
