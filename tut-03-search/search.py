a172c0d8303543cd2cb495ea6653ad3327e8a0aa
WonYeongjun <1014wonyoung@snu.ac.kr>
2024-04-10 12:52:19 +0900

    conflict solving
    Signed-off-by: WonYeongjun <1014wonyoung@snu.ac.kr>

--- tut-03-search/search.py
+++ tut-03-search/search.py
@@ -1,13 +1,10 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 # Load the 8x8 binary image from the CSV file
 image = np.loadtxt("square.csv", delimiter=",")
 # Ensure the loaded data is of integer type for display
 image = image.astype(int)
-blacklist=[]
-for y in range(image.shape[0]):
-	for x in range(image.shape[1]):
-		if image[y, x] == 0:
-			print("(%d,%d)" % (x,y))
-			blacklist.append((x,y))
-print(len(blacklist))
+# Display the image
+plt.imshow(image)
+plt.title('8x8 Binary Image with Random Pixel Values')
+plt.show()
